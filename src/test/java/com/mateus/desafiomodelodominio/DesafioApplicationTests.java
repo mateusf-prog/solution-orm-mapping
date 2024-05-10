17dcf7d18ae0befd8cc7f836d26efa43d39fc6ff
mateusfprog <mateus102006@hotmail.com>
2024-05-09 22:03:53 -0300

    create project

---
+++ src/test/java/com/mateus/desafiomodelodominio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.desafiomodelodominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
